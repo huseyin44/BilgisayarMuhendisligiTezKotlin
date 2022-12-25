41f02da330e932c4ca01b36702bf37d25a215820
Huseyin <huseyin-545@hotmail.com>
2022-12-26 00:43:13 +0300

    public ve personal chat guncel kullanciya gore metnin rengi degistirme eklendi

--- app/src/main/java/com/example/huseyinoral_bilgisayarmuhendisligitez/adapter/PersonalRecyclerAdapter.kt
+++ app/src/main/java/com/example/huseyinoral_bilgisayarmuhendisligitez/adapter/PersonalRecyclerAdapter.kt
@@ -3,12 +3,15 @@ package com.example.huseyinoral_bilgisayarmuhendisligitez.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.huseyinoral_bilgisayarmuhendisligitez.R
 import com.example.huseyinoral_bilgisayarmuhendisligitez.model.PersonalChatData
 import com.example.huseyinoral_bilgisayarmuhendisligitez.model.UserPublicChatData
+import com.google.firebase.auth.FirebaseAuth
 
 class PersonalRecyclerAdapter(val personalChatMessageList:ArrayList<PersonalChatData>): RecyclerView.Adapter<PersonalRecyclerAdapter.PersonalListViewHolder>() {
     class PersonalListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -23,10 +26,16 @@ class PersonalRecyclerAdapter(val personalChatMessageList:ArrayList<PersonalChat
     }
 
     override fun onBindViewHolder(holder: PersonalListViewHolder, position: Int) {
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
         holder.itemView.findViewById<TextView>(R.id.personalRecycler_isimsoyisim).text=personalChatMessageList[position].username
         holder.itemView.findViewById<TextView>(R.id.personalRecycler_text).text=personalChatMessageList[position].text
         val url=personalChatMessageList[position].userphotourl
         Glide.with(holder.itemView.context).load(url).into(holder.itemView.findViewById(R.id.personalRecycler_profileimage))
+        if(userID==personalChatMessageList[position].fromUserId){
+            holder.itemView.findViewById<TextView>(R.id.personalRecycler_isimsoyisim).setTextColor(holder.itemView.resources.getColor(R.color.holo_green_light))
+        }
+
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/huseyinoral_bilgisayarmuhendisligitez/adapter/UserPublicRecyclerAdapter.kt
+++ app/src/main/java/com/example/huseyinoral_bilgisayarmuhendisligitez/adapter/UserPublicRecyclerAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.huseyinoral_bilgisayarmuhendisligitez.R
 import com.example.huseyinoral_bilgisayarmuhendisligitez.model.UserPublicChatData
+import com.google.firebase.auth.FirebaseAuth
 
 class UserPublicRecyclerAdapter(val publicChatMessageList:ArrayList<UserPublicChatData>): RecyclerView.Adapter<UserPublicRecyclerAdapter.UserPublicListViewHolder>(){
     class UserPublicListViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
@@ -22,10 +23,15 @@ class UserPublicRecyclerAdapter(val publicChatMessageList:ArrayList<UserPublicCh
     }
 
     override fun onBindViewHolder(holder: UserPublicListViewHolder, position: Int) {
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
         holder.itemView.findViewById<TextView>(R.id.userpublicRecycler_isimsoyisim).text=publicChatMessageList[position].username
         holder.itemView.findViewById<TextView>(R.id.userpublicRecycler_text).text=publicChatMessageList[position].text
         val url=publicChatMessageList[position].userphotourl
         Glide.with(holder.itemView.context).load(url).into(holder.itemView.findViewById(R.id.userpublicRecycler_profileimage))
+
+        if(userID==publicChatMessageList[position].userid){
+            holder.itemView.findViewById<TextView>(R.id.userpublicRecycler_isimsoyisim).setTextColor(holder.itemView.resources.getColor(R.color.holo_green_light))
+        }
     }
 
     override fun getItemCount(): Int {
